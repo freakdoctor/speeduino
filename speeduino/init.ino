dd9317a73e5f8f82184f16396b686286692a68f9
freakdoctor <69394610+freakdoctor@users.noreply.github.com>
2021-10-10 21:54:38 +0200

    tb pwm pin
    tb pwm pin analog to digital correction in init.ino

--- speeduino/init.ino
+++ speeduino/init.ino
@@ -2543,7 +2543,7 @@ void setPinMapping(byte boardID)
   if ( (configPage10.vvt2Pin != 0) && (configPage10.vvt2Pin < BOARD_MAX_IO_PINS) ) { pinVVT_2 = pinTranslate(configPage10.vvt2Pin); }
 
   if ( (configPage13.pedalPositionEnable) && (configPage13.pedalPositionPin < BOARD_MAX_IO_PINS) ) { pinPedalPosition = pinTranslateAnalog(configPage13.pedalPositionPin); }
-  if ( (configPage13.pedalPositionEnable) && (configPage13.tbMotPwmPin < BOARD_MAX_IO_PINS) ) { pinTbMotPwm = pinTranslateAnalog(configPage13.tbMotPwmPin); }
+  if ( (configPage13.pedalPositionEnable) && (configPage13.tbMotPwmPin < BOARD_MAX_IO_PINS) ) { pinTbMotPwm = pinTranslate(configPage13.tbMotPwmPin); }
 
   //Currently there's no default pin for Idle Up
   pinIdleUp = pinTranslate(configPage2.idleUpPin);
